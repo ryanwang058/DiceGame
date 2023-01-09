da537112e9c95b16830a166a4fc305ca36721720
ryanwang058 <ryanwang5@cmail.carleton.ca>
2023-01-09 15:25:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Dice Game
+
+### Game Rules (English)
+
+
+#### 1. Setting the Score
+
+- A Player enters the target score at "FINAL SCORE" and clicks "SET SCORE" to complete the setting. Any game requires a score setting before it starts.
+
+#### 2. Game Starts
+
+- Player clicks "ROLL DICE" to roll two dices at a time, and the sum of the points of the dices is the current score.
+
+- If Player chooses to continue rolling dices (click "ROLL DICE"), the current score will be accumulated until either point of the two dices is 1. At this time, Player clicks "CONFIRM" and the current score will be reset to zero. Next, it's the other Player's turn to roll the dices.
+
+- If Player chooses to hold the current score (click "HOLD"), the current score will be added to the total score and then be cleared. Next, it's the other Player's turn to roll the dices.
+
+#### 3. Game Over
+
+- Any Player reaches the target score wins. Player clicks "NEW GAME" to start a new game.
+
+> Updates: 
+> 1.  When rolling to 1 point, the dices'points are shown.
+> 2.  Added a "CONFIRM" button, Player now has to confirm the clear of current scores before the other Player takes turn to play.
+
+[Game Link]()
---
+++ app.js
@@ -0,0 +1,195 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying, lastDice, gameSet, winningScore, lostConfirm;
+
+init();
+
+// Set player 1/2's current dice point
+// - this could only change text content, not HTML, so it can't output italic numbers
+// Here if we want to change HMTL:
+// - document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// function btn() {
+//   // do sth here
+// }
+// btn();
+
+// call-back function
+// document.querySelector('.btn-roll').addEventListener('click',btn);
+
+// click to roll dices
+document.querySelector('.btn-roll').addEventListener('click',function() {
+  if (gamePlaying && gameSet && lostConfirm) {
+    // 1. Random number
+    // var dice = Math.floor(Math.random() * 6) + 1;
+
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    
+    // querySelector only selects the first element;
+    // need to use for loop to implement on several elements with the same class
+
+    // var diceDOM = document.querySelector('.dice');
+    // diceDOM.style.display = 'block';
+
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice1 !== 1 && dice2 !== 1) {
+      // Add score
+      roundScore += dice1 + dice2;
+      lostConfirm = true;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore; // textContent
+    } else {
+      lostConfirm = false;
+      // Send notifications
+      document.querySelector('.player-' + activePlayer +'-lost').style.display = 'block';
+
+    }
+    // if (lastDice === dice && dice === 6) {
+    //   scores[activePlayer] = 0;
+    //   // display clearing to 0
+    //   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //   nextPlayer();
+    // } else {
+    //   lastDice = dice;
+    // }
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  if (gamePlaying && gameSet && lostConfirm) {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // Check if player won the game
+    if (scores[activePlayer] >= winningScore) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+      // gameSet = false;
+    } else {
+      // Next player
+      nextPlayer();
+    }
+  }
+
+});
+
+document.querySelector('.btn-set').addEventListener('click',function() {
+  var input = document.querySelector('.final-score').value;
+  // Verify the input
+  if (input > 0 && input <= 999) {
+    winningScore = input;
+    gameSet = true;
+    
+    // Change styles
+    document.querySelector('.btn-set').classList.add('btnStrong');
+    document.querySelector('.ion-ios-settings').style.display='none';
+    document.querySelector('.txt-set').textContent = 'Score set!';
+  }
+});
+
+document.querySelector('.btn-0-yes').addEventListener('click',function() {
+  nextPlayer();
+});
+
+document.querySelector('.btn-1-yes').addEventListener('click',function() {
+  nextPlayer();
+});
+
+function nextPlayer() {
+  // Remove lost messages
+  document.querySelector('.player-' + activePlayer +'-lost').style.display = 'none';
+
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  lostConfirm = true;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlaying = true;
+  lostConfirm = true;
+  gameSet = false;
+  
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+
+  // Eliminate set winning score
+  document.querySelector('.final-score').value = '';
+
+  // Remove set scores
+  document.querySelector('.btn-set').classList.remove('btnStrong');
+  document.querySelector('.txt-set').textContent = 'Set score';
+  document.querySelector('.ion-ios-settings').style.display='inline-block';
+
+  // Remove lost messages
+  document.querySelector('.player-0-lost').style.display = 'none';
+  document.querySelector('.player-1-lost').style.display = 'none';
+
+}
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+/**
+ * 1. rolls two 6 points -> lose entire score
+ * (save precious dice roll)
+ *  
+ * 2.add an input field in HTML to set winning score (JS: .value to read the value)
+ * 
+ * 3. add another dice, loose current score with 1 dice of point 1(, but can continue with the other one)
+ */
+
+
